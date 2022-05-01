9eb8b39270d1c3fb226da72a29c6fc884fd9e354
Mostapha el amine BENLAGHA <32815384+Mostafaben@users.noreply.github.com>
2022-05-01 19:44:56 +0100

    remove app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { PostsModule } from './posts/posts.module';
@@ -9,6 +8,5 @@ import { TreeModule } from './tree/tree.module';
 @Module({
   imports: [PrismaModule, UserModule, PostsModule, TreeModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
